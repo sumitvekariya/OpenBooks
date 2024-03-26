2f9c8aa4e103c82d4d1b924759d9143b1df50448
shyam <faltulal369@gmail.com>
2024-03-27 04:53:43 +0530

    fix book detail page ui

--- lib/Screens/bookdetails.dart
+++ lib/Screens/bookdetails.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:openbook/utils/globalvar.dart';
 
 import '../Models/book_model.dart';
+import '../utils/globalvar.dart';
 
 class BookDetails extends StatefulWidget {
   final Book book;
@@ -21,24 +21,24 @@ class _BookDetailsState extends State<BookDetails> {
         child: Column(
           children: [
             Container(
-              margin: EdgeInsets.fromLTRB(10.w, 5.h, 10.w, 30.h),
-              height: screenHeight! * 0.40,
+              margin: EdgeInsets.fromLTRB(10.w, 10.h, 10.w, 20.h),
+              height: 350.h,
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      const Spacer(),
-                      Text(
-                        "By : ${widget.book.username}",
-                        style: TextStyle(
-                          fontFamily: globalfontfamily,
-                          fontWeight: FontWeight.w300,
-                          fontSize: 10.sp,
-                          color: Colors.blue[800],
-                        ),
-                      ),
-                    ],
-                  ),
+                  // Row(
+                  //   children: [
+                  //     const Spacer(),
+                  //     Text(
+                  //       "owned by : ${widget.book.username}",
+                  //       style: TextStyle(
+                  //         fontFamily: globalfontfamily,
+                  //         fontWeight: FontWeight.w300,
+                  //         fontSize: 10.sp,
+                  //         color: Colors.blue[800],
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
                   SizedBox(height: 15.h),
                   Center(
                     child: Card(
@@ -55,48 +55,45 @@ class _BookDetailsState extends State<BookDetails> {
                 ],
               ),
             ),
-            Stack(
-              alignment: Alignment.topCenter,
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 15.h),
-                  height: screenHeight! * 0.509,
-                  width: screenWidth! * 0.95,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black12),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(32.r),
-                      topRight: Radius.circular(32.r),
-                    ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 15.h),
+                height: MediaQuery.of(context).size.height - 350.h, //- kToolbarHeight,
+                width: 370.w,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black12),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(32.r),
+                    topRight: Radius.circular(32.r),
                   ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Text(
-                          widget.book.bookName,
-                          style: TextStyle(fontFamily: globalfontfamily, fontWeight: FontWeight.w800, fontSize: 26.sp),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text(
+                        widget.book.bookName,
+                        style: TextStyle(fontFamily: globalfontfamily, fontWeight: FontWeight.w800, fontSize: 26.sp),
+                      ),
+                      SizedBox(height: 5.h),
+                      Text(
+                        widget.book.authorName,
+                        style: TextStyle(fontFamily: globalfontfamily, fontWeight: FontWeight.w500, fontSize: 20.sp),
+                      ),
+                      SizedBox(height: 15.h),
+                      Text(
+                        widget.book.bookdesc,
+                        style: TextStyle(
+                          fontFamily: globalfontfamily,
+                          fontWeight: FontWeight.w300,
+                          fontSize: 13.sp,
+                          color: Colors.grey,
                         ),
-                        SizedBox(height: 5.h),
-                        Text(
-                          widget.book.authorName,
-                          style: TextStyle(fontFamily: globalfontfamily, fontWeight: FontWeight.w500, fontSize: 20.sp),
-                        ),
-                        SizedBox(height: 15.h),
-                        Text(
-                          widget.book.bookdesc,
-                          style: TextStyle(
-                            fontFamily: globalfontfamily,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 13.sp,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                )
-              ],
+                ),
+              ),
             ),
           ],
         ),
